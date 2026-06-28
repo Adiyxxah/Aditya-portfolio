aed734ac4ae51c773dcd1124af34f99cdc23f042
Aditya Kumar Pradhan <pradhan.adi586@gmail.com>
2026-06-28 13:31:30 +0530

    DevOps Engineer

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ void main() {
 //  YOUR PERSONAL INFO
 // ══════════════════════════════════════════════════
 const String kName     = 'Aditya Kumar Pradhan';
-const String kRole     = '📊 DevOps Engineer  • Flutter Developer';
+const String kRole     = 'DevOps Engineer • Flutter Developer';
 const String kTagline  = 'Turning raw data into insights that drive decisions.';
 const String kEmail    = 'pradhan.adi586@gmail.com';
 const String kPhone    = '+91-7326886586';
